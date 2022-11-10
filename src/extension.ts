2e24b90702f45ed0f22f646e0f04c9cca1be0d97
rvcas <x@rvcas.dev>
2022-11-10 01:09:08 -0500

    feat: start the language server client on activate

--- src/extension.ts
+++ src/extension.ts
@@ -1,23 +1,48 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from "vscode";
+import {
+  LanguageClient,
+  LanguageClientOptions,
+  ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
+
+let client: LanguageClient | undefined;
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log("aiken is now active!");
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  //   let disposable = vscode.commands.registerCommand("thing.helloWorld", () => {
-  // The code you place here will be executed every time your command is executed
-  // Display a message box to the user
-  // vscode.window.showInformationMessage("Hello World from thing!");
-  //   });
-  //   context.subscriptions.push(disposable);
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "aiken" }],
+    synchronize: {
+      fileEvents: [
+        vscode.workspace.createFileSystemWatcher("**/aiken.toml"),
+        vscode.workspace.createFileSystemWatcher("**/manifest.toml"),
+      ],
+    },
+  };
+
+  let serverOptions: ServerOptions = {
+    command: "aiken",
+    args: ["lsp"],
+    transport: TransportKind.stdio,
+    options: {
+      env: Object.assign(process.env, {}),
+    },
+  };
+
+  client = new LanguageClient(
+    "aiken_language_server",
+    "Aiken Language Server",
+    serverOptions,
+    clientOptions
+  );
+
+  client.start();
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  return client?.stop();
+}
